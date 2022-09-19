da2eec5661383d96c170b9c36629de5834d3c35d
Mathieu Guigue <mgguigue@gmail.com>
2022-09-19 14:00:03 +0200

    Original ToolFramework won't work without fix

--- hkinstall.py
+++ hkinstall.py
@@ -12,7 +12,7 @@ class MyHKLib(CMake):
         self._package_name = "hk-ToolFrameworkCore"
         self._package_version = "1.1.0"
 
-        self._git_url = "git@github.com:ToolFramework/ToolFrameworkCore.git"
+        self._git_url = "git@github.com:guiguem/ToolFrameworkCore.git"
         self._git_branch_tag = "main"
         self._git_clone_dir = "src"
 
